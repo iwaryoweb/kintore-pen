ba06ce8da7c0b72ab21646afd0170e23c5818219
iwaryoweb <iwaryoweb@gmail.com>
2026-08-31 01:27:58 +0900

    v1.1: カレンダーUI（記録日にドット・過去日の追記・その日の記録を読む、SWキャッシュv2）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kintore-pen-v1';
+const CACHE = 'kintore-pen-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
